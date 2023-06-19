5669af790613857103961eb63b73b30b14fac337
Oren <orenz0@protonmail.com>
2023-06-20 00:01:21 +0300

    bookie.tickets is deprecated
    old migrations from the original events
    project are irrelevant here.

--- migrations.py
+++ migrations.py
@@ -18,22 +18,6 @@ async def m001_initial(db):
     """
     )
 
-    await db.execute(
-        """
-        CREATE TABLE bookie.tickets (
-            id TEXT PRIMARY KEY,
-            wallet TEXT NOT NULL,
-            competition TEXT NOT NULL,
-            name TEXT NOT NULL,
-            reward_target TEXT NOT NULL,
-            registered BOOLEAN NOT NULL,
-            time TIMESTAMP NOT NULL DEFAULT """
-        + db.timestamp_now
-        + """
-        );
-    """
-    )
-
 
 async def m002_changed(db):
 
@@ -53,29 +37,3 @@ async def m002_changed(db):
         );
     """
     )
-
-    for row in [list(row) for row in await db.fetchall("SELECT * FROM bookie.tickets")]:
-        usescsv = ""
-
-        for i in range(row[5]):
-            if row[7]:
-                usescsv += "," + str(i + 1)
-            else:
-                usescsv += "," + str(1)
-        usescsv = usescsv[1:]
-        await db.execute(
-            """
-            INSERT INTO bookie.ticket (
-                id,
-                wallet,
-                competition,
-                name,
-                reward_target,
-                registered,
-                paid
-            )
-            VALUES (?, ?, ?, ?, ?, ?, ?)
-            """,
-            (row[0], row[1], row[2], row[3], row[4], row[5], True),
-        )
-    await db.execute("DROP TABLE bookie.tickets")
