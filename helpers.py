a695d3b8a81cff103c1121571671bae1497875e2
Oren <orenz0@protonmail.com>
2023-07-01 22:38:01 +0300

    pay lnurlp

--- helpers.py
+++ helpers.py
@@ -1,7 +1,9 @@
-from urllib.parse import urlparse
+from typing import Dict, Optional
+from urllib.parse import urlparse, quote
 import json
 
-from lnbits import lnurl
+from lnbits import lnurl, bolt11
+from lnbits.core.services import fee_reserve, pay_invoice
 import httpx
 from .models import LnurlpParameters
 
@@ -20,8 +22,11 @@ async def get_lnurlp_parameters(code: str) -> LnurlpParameters:
                 + name
             )
         else:
-            raise Exception("Unparseable lnurlp or lightning-address")
-    parsed_url = urlparse(url)
+            raise Exception("Malformed lnurlp or lightning-address")
+    try:
+        parsed_url = urlparse(url)
+    except:
+        raise Exception("Unparsable lnurlp or lightning-address")
     if "tag=login" in parsed_url.query.split("&"):
         raise Exception("Invalid lnurlp - this is a login lnurl")
     async with httpx.AsyncClient() as client:
@@ -53,4 +58,53 @@ async def get_lnurlp_parameters(code: str) -> LnurlpParameters:
         callback=callback,
         commentAllowed=commentAllowed if isinstance(commentAllowed, int) else 0,
     )
+
+async def pay_lnurlp(wallet_id: str, code: str, amount_msat: int, description: str, extra: Optional[Dict]):
+    # Deduct lightning fees
+    # This may actually deduct too much, because the final fee will be
+    # fee_reserve(final_amount_msat) <= fee_reserve(amount_msat), but the exact calculation
+    # might get complicated if fee_reserve changes.
+    final_amount_msat = amount_msat - fee_reserve(amount_msat)
+    params = await get_lnurlp_parameters(code)
+    if final_amount_msat < params.minSendable:
+        raise Exception("Payment is too small for receiver")
+    if final_amount_msat > params.maxSendable:
+        raise Exception("Payment is too high for receiver")
+    try:
+        parsed_callback = urlparse(params.callback)
+    except:
+        raise Exception("Unparsable lnurlp callback")
+    comment = description if len(description) <= params.commentAllowed else ""
+    full_callback_url = params.callback + ("&" if parsed_callback.query else "?") + f"amount={amount_msat}" + (
+        f"&comment={quote(comment)}" if comment else ""
+    )
+    async with httpx.AsyncClient() as client:
+        r = await client.get(full_callback_url, timeout=5)
+        if r.is_error:
+            raise Exception("Failed to call callback url")
+    try:
+        data = json.loads(r.text)
+    except json.decoder.JSONDecodeError:
+        raise Exception("Failed to decode callback response json")
+    if not isinstance(data, dict):
+        raise Exception("Failed to decode callback response object")
+    pr = data.get("pr")
+    if not isinstance(pr, str):
+        raise Exception("Unexpected callback response parameters types")
+    try:
+        decoded_payment_request = bolt11.decode(pr)
+    except:
+        raise Exception("Failed to parse bolt11 payment request")
+    if decoded_payment_request.amount_msat > final_amount_msat:
+        raise Exception("Amount in invoice is higher than requested")
+    # Should we check invoice description?
+    payment_hash = await pay_invoice(
+        wallet_id=wallet_id,
+        payment_request=pr,
+        description=description,
+        extra=extra,
+        max_sat=(amount_msat // 1000) + 1, # safety
+    )
+    return payment_hash
+
 
