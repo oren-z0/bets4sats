4f6112540c6654b2ba5237aa2e9b2d54d79b1912
Oren <orenz0@protonmail.com>
2023-07-05 21:01:21 +0300

    logs

--- tasks.py
+++ tasks.py
@@ -4,6 +4,7 @@ import json
 from lnbits.core.models import Payment
 from lnbits.helpers import get_current_extension_name
 from lnbits.tasks import register_invoice_listener
+from loguru import logger
 
 from .crud import cas_competition_state, get_ticket, cas_ticket_state, get_competition, update_ticket, is_competition_payment_complete
 from .helpers import pay_lnurlp, send_ticket
@@ -38,30 +39,37 @@ async def on_invoice_paid(payment: Payment) -> None:
     return
 
 async def wait_for_reward_ticket_ids():
+    logger.info("wait_for_reward_ticket_ids: started")
     while True:
         ticket_id = await reward_ticket_ids_queue.get()
         await on_reward_ticket_id(ticket_id)
 
 async def on_reward_ticket_id(ticket_id: str) -> None:
+    logger.info("on_reward_ticket_id: called", ticket_id)
     ticket = await get_ticket(ticket_id)
     if not ticket:
+        logger.warning("on_reward_ticket_id: ticket not found, deleted before handled?", ticket_id)
         return
+    logger.info("on_reward_ticket_id: handling ticket:", ticket)
     new_state = {
         "WON_UNPAID": "WON_PAYING",
         "WON_PAYMENT_FAILED": "WON_PAYING",
         "CANCELLED_UNPAID": "CANCELLED_PAYING",
         "CANCELLED_PAYMENT_FAILED": "CANCELLED_PAYING"
     }.get(ticket.state)
+    logger.info("on_reward_ticket_id: new state:", ticket_id, new_state)
     if not new_state:
         return
     cas_success = await cas_ticket_state(ticket_id, ticket.state, new_state)
     if not cas_success:
+        logger.info("on_reward_ticket_id: cas failed:", ticket_id)
         return
     final_reward_msat = 0
     try:
         # get ticket again
         ticket = await get_ticket(ticket_id)
         if not ticket:
+            logger.info("on_reward_ticket_id: failed to re-get ticket:", ticket_id)
             return
         if ticket.state == "CANCELLED_PAYING":
             reward_msat = ticket.amount * 1000
@@ -72,6 +80,7 @@ async def on_reward_ticket_id(ticket_id: str) -> None:
             total_msat = sum(choice["total"] for choice in choices) * 1000
             reward_msat = total_msat * ticket.amount * (100 - PRIZE_FEE_PERCENT) // (choices[ticket.choice]["total"] * 100)
             description_prefix = "BookieReward"
+        logger.info("on_reward_ticket_id: paying lnurlp:", ticket_id)
         payment_hash, final_reward_msat = await pay_lnurlp(
             ticket.wallet,
             ticket.reward_target,
@@ -80,6 +89,7 @@ async def on_reward_ticket_id(ticket_id: str) -> None:
             {"tag":"bookie"}
         )
     except Exception as exception:
+        logger.warning("on_reward_ticket_id: failed:", ticket_id, exception)
         await update_ticket(
             ticket_id,
             state={
@@ -89,6 +99,7 @@ async def on_reward_ticket_id(ticket_id: str) -> None:
             reward_failure=str(exception)
         )
     else:
+        logger.warning("on_reward_ticket_id: updating ticket to paid:", ticket_id)
         await update_ticket(
             ticket.id,
             state={
@@ -100,6 +111,7 @@ async def on_reward_ticket_id(ticket_id: str) -> None:
             reward_payment_hash=payment_hash
         )
     competition_complete = await is_competition_payment_complete(ticket.competition)
+    logger.warning("on_reward_ticket_id: competition_complete:", ticket_id, competition_complete)
     if competition_complete:
         await cas_competition_state(
             ticket.competition,
